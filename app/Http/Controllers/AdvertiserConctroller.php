8fce9734d8c09bd80e18dfe8bb7545646d1f7b9d
jervi <jervi>
2022-04-02 15:10:01 +0200

    advertiser api

--- app/Http/Controllers/AdvertiserConctroller.php
+++ app/Http/Controllers/AdvertiserConctroller.php
@@ -14,64 +14,123 @@ class AdvertiserConctroller extends Controller
 {
     //request a registery as an Advertiser
     public function become(Request $request) {
-        //[username, email, password, userdetails]
-        //[companyName, companyDetails, images, phone_number]
+        //* [name, email, password, userdetails]
+        //* [companyName, companyDetails, images, phone_number]
 
-        $user = new User();
-        $user->role_id = Role::where('name', 'advertiser')->first()->id;
-        $user->name = $request->username;
-        $user->email = $request->email;
-        $user->password = Hash::make($request->password);
-        $user->details = $request->userdetails ? $request->userdetails : null;
-        $user->save();
+
+        $fields = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|string',
+            'password' => 'required|string',
+            //TODO: password confirmed if statement
+            'password_confirmed' => 'required|string',
+            'userdetails' => 'string',
+            'companyName' => 'required|string',
+            'companyDetails' => 'string',
+            'phone_number' => 'string',
+            //TODO: 'images' => compress file
+            'images' => 'string'
+        ]);
+
+        //if user exists
+        $user = User::where('email', $request->email)->first();
+
+
+        if($user) {
+            //check the password if correct
+            if(!$user || !Hash::check($fields['password'], $user->password)) {
+                return response([
+                    'message' => 'Bad creds'
+                ], 401);
+            }
+        }
+        //Doesnt exist
+        else {
+            $user = new User();
+            $user->role_id = Role::where('name', 'advertiser')->first()->id;
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->password = Hash::make($request->password);
+            $user->details = $request->userdetails ? $request->userdetails : null;
+            $user->save();
+        }
+        //create token
+        $token = $user->createToken($fields['email'])->plainTextToken;
 
         $adv = new Advertiser();
         $adv->user_id = $user->id;
         $adv->name = $request->companyName;
         $adv->details = $request->companyDetails;
-        $adv->images = $request->images;
+        $adv->images = '$request->images';
         $adv->phone_number = $request->phone_number;
         $adv->save();
 
-        return [$adv, $user];
+        return [
+            'advertiser' => $adv,
+            'user' => $user,
+            'token' => $token
+        ];
     }
 
     public function addOffer(Request $request) {
+        //TODO: limit ammount of offers to 7
         //[template_id, amount_tickets, images, details]
         //[campaign_starts, campaign_ends]
-
         $offer = new Offer();
-        $offer->advertiser_id = Auth()->user->advertiser()->first()->id;
-        $offer->template = $request->template_id;
-        $offer->tickets_left = $request->amount_tickets;
+        $offer->advertiser_id = Auth::user()->advertiser()->first()->id;
+        $offer->template_id = intval($request->template_id);
+        $offer->tickets_left = intval($request->amount_tickets);
         $offer->images = $request->images;
         $offer->details = $request->details;
 
-        $offer->campaign_starts = $request->campaign_starts;
-        $offer->campaign_ends = $request->campaign_ends;
+        $offer->campaign_starts = date('Y-m-d',strtotime($request->campaign_starts));
+        $offer->campaign_ends = date('Y-m-d',strtotime($request->campaign_ends));
 
+        //TODO: $offer->campaign_name = $request->campaign_name;
         $offer->save();
 
-        return $offer;
+        return [
+            'offer' => $offer,
+        ];
     }
 
-    public function manageOffers() {
+    public function manageOffers(Request $request) {
+        $offers = Auth::user()->advertiser()->first()->offers()->get();
 
+        return [
+            'offer' => $offers,
+        ];
     }
 
     public function showOffer($id) {
-        $advertiser = Auth()->user;
+        $advertiser = Auth::user()->advertiser()->first();
         $offers = $advertiser->offers();
         $offer = $offers->find($id);
 
-        return $offer;
+        return [
+            'offer' => $offer,
+        ];
     }
 
-    public function updateOffer(Request $request) {
+    public function deleteOffer($id, Request $request) {
 
-    }
+        //? check if advertiser is the owner
+        $advertiser = Auth::user()->advertiser()->first();
+        $offer = Offer::find($id);
+        if($offer->advertiser_id != $advertiser->id) {
+            return response([
+                'message' => 'you are not the owner'
+            ], 401);
+        }
 
-    public function deleteOffer(Request $request) {
+
+        $offer->delete();
+
+        return [
+            'message' => 'offer deleted',
+        ];
+
+
 
     }
 
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
-use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\AdvertiserConctroller;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,8 +20,25 @@ use Illuminate\Support\Facades\Route;
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 
-//protected routes
 Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::post('/logout', [AuthController::class, 'logout']);
+});
+
+//Advertisers
+//become an advertise, post a request
+Route::post('/becomeAdvertiser', [AdvertiserConctroller::class, 'become']);
+//protected routes
+//TODO: if advertiser
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    //add an offer
+    Route::post('/advertiser/addOffer', [AdvertiserConctroller::class, 'addOffer']);
+    //delete an offer
+    Route::post('/advertiser/deleteOffer/{id}', [AdvertiserConctroller::class, 'deleteOffer']);
+    //manager offers
+    Route::get('/advertiser/managerOffers', [AdvertiserConctroller::class, 'manageOffers']);
+    //show an offer
+    Route::get('/advertiser/showOffer/{id}', [AdvertiserConctroller::class, 'showOffer']);
+    //update an offer
+    //! not working Route::post('/advertiser/changeOffer', [AdvertiserConctroller::class], 'change');
 });
 
